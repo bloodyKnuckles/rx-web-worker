548ef1b1fca1996d48a8f102f25920e258a4c3e6
bloodyKnuckles <treenopie@gmail.com>
2016-05-06 18:06:50 -0500

    Added return patch channel.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+var patchDOM = require('virtual-dom/patch')
+var fromJSON = require('vdom-as-json/fromJson')
+
 console.log('main init')
+var rootnode = document.documentElement
 
 var worker = new Worker("./workerB.js")
 worker.addEventListener('message', function (evt) {
@@ -6,10 +10,18 @@ worker.addEventListener('message', function (evt) {
   evt.data.forEach(function (cmd) {
     switch ( cmd.cmd ) {
       case 'event': workerHandler(cmd.event); break
+      case 'patch': patchHandler(fromJSON(cmd.patch)); break;
     }
   })
 }, false)
-worker.postMessage({cmd: 'init', absurl: location.origin + location.pathname})
+//worker.postMessage({cmd: 'init', absurl: location.origin + location.pathname})
+
+function patchHandler (patch) {
+console.log(patch)
+  window.requestAnimationFrame(function () {
+    //patchDOM(rootnode, patch)
+  })
+}
 
 function workerHandler (evtinfo) {
   document.querySelector(evtinfo.element).addEventListener(evtinfo.event, function (evt) {
--- worker.js
+++ worker.js
@@ -2,6 +2,25 @@ Rx = require('rx-lite')
 
 console.log('worker init')
 
+var domevent$ = Rx.Observable.fromEvent(self, 'message', function (evt) {
+  return evt.data
+})
+
+var buttonclick$ = domevent$.filter(function (cmd) {
+  return 'event' === cmd.cmd &&
+    ('button#down' === cmd.event.element || 'button#up' === cmd.event.element)
+})
+  .map(function (cmd) { return parseInt(cmd.event.response, 10) })
+  .scan(function (prev, next) { return prev + next })
+
+var clicksub = buttonclick$.subscribe(
+  function (clicktotal) {
+    patchDOM('patch ' + clicktotal)
+  },
+  function (err) { console.log('Error: %s', err) },
+  function () { console.log('Completed') }
+)
+
 self.postMessage(
   [
     {cmd: 'event', 'event': {'element': 'button#down', 'event': 'click', 'response': 'target.value'}},
@@ -9,39 +28,6 @@ self.postMessage(
   ]
 )
 
-//*
-var source = Rx.Observable.fromEvent(self, 'message', function (evt) {
-  return evt.data
-})
-var subscription = source.subscribe(
-  function (cmd) {
-    //console.log('Next: Clicked! ', cmd)
-    cmdOp(cmd)
-  },
-  function (err) {
-    console.log('Error: %s', err)
-  },
-  function () {
-    console.log('Completed')
-  }
-)
-//*/
-
-/*
-self.addEventListener('message', function (evt) {
-  cmdOp(evt.data)
-}, false)
-//*/
-
-function cmdOp (cmd) {
-  switch ( cmd.cmd ) {
-    case 'echo':  self.postMessage({cmd: 'echo', msg: data}); break
-    case 'event':
-      console.log('worker ', cmd.event);
-      switch ( cmd.event.element ) {
-        case 'button': console.log(cmd.event.response); break;
-        case 'button': console.log(cmd.event.response); break;
-      }
-      break
-  }
+function patchDOM (patch) {
+  self.postMessage([{cmd: 'patch', 'patch': patch}])
 }
